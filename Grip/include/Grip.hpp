020cff6b6867a16e22629ea3abbb7fe801984bc3
NishiiTakahiro <nishiitakahiro8888@gmail.com>
2017-03-07 01:05:18 +0900

    glmのインクルードパス設定

--- Grip/include/Grip.hpp
+++ Grip/include/Grip.hpp
@@ -1,7 +1,16 @@
 ﻿#pragma once
 
-#define GLM_FORCE_CXX14
-//#include <glm/glm.hpp>
+#pragma warning( push )
+#pragma warning( disable : 4201)
+
+//#define GLM_FORCE_CXX98
+//#define GLM_FORCE_CXX03
+//#define GLM_FORCE_CXX11
+//#define GLM_FORCE_CXX14
+#include <glm/glm.hpp>
+
+#pragma warning( pop ) 
+
 
 
 #include "Game.hpp"
